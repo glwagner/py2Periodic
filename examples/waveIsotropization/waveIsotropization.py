7ae9e03396393c9955d1bbc4e07af2d5919f712b
glwagner <wagner.greg@gmail.com>
2017-04-24 06:07:38 -0700

    Improving isotropization example

--- examples/waveIsotropization/waveIsotropization.py
+++ examples/waveIsotropization/waveIsotropization.py
@@ -3,93 +3,60 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 
-sys.path.append('../py2Periodic/')
+sys.path.append('../../')
 from py2Periodic.physics import hydrostaticWaveEqn_xy
 from numpy import pi
-
-# Parameters:
-# * Physical
-f0 = 1e-4
-alpha = 1
-sigma = f0*np.sqrt(1+alpha)
-kappa = 32.0*pi / (Lx*np.sqrt(alpha))
-# * Numerical
-dt = 0.05 * 2.0*pi/f0
-# * Frictional
-(waveVisc, waveViscOrder) = (1e24, 8.0)
 
 # Load data
 fileName = 'strongTurbulentInitialConditions.hdf5'
 runName = 'ic_00'
 
 dataFile = h5py.File(fileName, 'r')
-run = dataFile[runName]
+turbData = dataFile[runName]
+
+# Extract initial condition
+q0 = turbData['q_snapshots']['q'][:, :, -1]
 
 # Generate dictionary of parameters
-params = { param:value for param, value in run.attrs.iteritems() }
+turbParams = { param:value for param, value in turbData.attrs.iteritems() }
+
+# Parameters:
+Lx = turbParams['Lx']
+alpha = 1
+f0 = 1e-4
+sigma = f0*np.sqrt(1+alpha)
+kappa = 32.0*pi / (Lx*np.sqrt(alpha))
+dt = 0.05 * 2.0*pi/f0
+(waveVisc, waveViscOrder) = (1e24, 8.0)
 
 # Add new parameters
 addParams = {
-    'meanVisc' : params['visc'], 
-    'meanViscOrder' : params['viscOrder'], 
-    'waveVisc' : waveVisc,
+    'meanVisc'      : turbParams['visc'], 
+    'meanViscOrder' : turbParams['viscOrder'], 
+    'waveVisc'      : waveVisc,
     'waveViscOrder' : waveViscOrder,
-    'f0' : f0,
-    'sigma' : sigma,
-    'kappa' = kappa, 
-    'dt' : dt, 
-    'timeStepper', 'ETDRK4', 
+    'f0'          : f0,
+    'sigma'       : sigma,
+    'kappa'       : kappa, 
+    'dt'          : dt, 
+    'timeStepper' : 'ETDRK4', 
 }
 
-del params['visc']
-del params['viscOrder']
+del turbParams['visc']
+del turbParams['viscOrder']
 
+params = turbParams.copy()
 params.update(addParams)
     
 # Initialize hydrostatic wave model
 hwe = hydrostaticWaveEqn_xy.model(**params)
-
 hwe.set_q(q0)
 
-# Run turbulence model
-stopTime = 400.0*pi/f0
+plt.figure('Initial condition'), plt.clf()
+plt.imshow(hwe.q)
+plt.show()
+
+stopTime = 256.0*pi/f0
 nSteps = int(np.ceil(stopTime / dt))
-nPlots = 10
-nSubsteps = int(np.ceil(nSteps/nPlots))
 
-print("Iterating {:d} times and making {:d} "
-        "plots:".format(nSteps, nPlots))
-for i in xrange(nPlots):
-
-    turb.run(nSteps=nSubsteps, nLogs=10)
-    turb.update_state_variables()
-
-    # Plot the result
-    plt.figure('Flow', figsize=(12, 8)); plt.clf()
-    plt.imshow(turb.q)
-    plt.pause(0.01)
-
-print("The root-mean-square vorticity is " + \
-        "{:0.3f}.\n".format(np.sqrt((turb.q**2.0).mean())))
-
-print("Finished the preliminary turbulence integration! " \
-        "Now let's put a wave field in it...")
-
-# Give final turbulence field to wave model, and run
-hwe.set_q(turb.q)
-
-stopTime = 200.0*pi/f0
-nSteps = int(np.ceil(stopTime / dt))
-nPlots = 100
-nSubsteps = int(np.ceil(nSteps/nPlots))
-
-print("Iterating {:d} times and making {:d} plots:".format(nSteps, nPlots))
-for i in xrange(nPlots):
-    
-    hwe.run(nSteps=nSubsteps, nLogs=2)
-    hwe.update_state_variables()
-    
-    plt.figure('Waves and flow', figsize=(12, 8)); plt.clf()
-    plt.subplot(121); plt.imshow(hwe.q)
-    plt.subplot(122); plt.imshow(np.sqrt(hwe.u**2.0 + hwe.v**2.0))
-    plt.pause(0.01)
+hwe.run(nSteps=nSteps, nPlots=128, nLogs=128)
